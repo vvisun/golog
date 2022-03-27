481eec27c3b8d4d5cb54b8959715c4cd43cf3b9e
hpLiu <374708234@qq.com>
2022-03-27 17:09:00 +0800

    添加全局日志级别配置

--- level.go
+++ level.go
@@ -23,6 +23,12 @@ var levelString = [...]string{
 	"[FATA]",
 }
 
+var globalLevel = Level_Debug
+
+func SetGlobalLevel(lvl Level) {
+	globalLevel = lvl
+}
+
 func str2loglevel(level string) Level {
 
 	switch level {
@@ -50,14 +56,23 @@ func SetLevelByString(loggerName string, level string) error {
 }
 
 func (slf *Logger) Debugf(format string, v ...interface{}) {
+	if slf.level < globalLevel {
+		return
+	}
 	slf.LogText(Level_Debug, fmt.Sprintf(format, v...), nil)
 }
 
 func (slf *Logger) Infof(format string, v ...interface{}) {
+	if slf.level < globalLevel {
+		return
+	}
 	slf.LogText(Level_Info, fmt.Sprintf(format, v...), nil)
 }
 
 func (slf *Logger) Warnf(format string, v ...interface{}) {
+	if slf.level < globalLevel {
+		return
+	}
 	slf.LogText(Level_Warn, fmt.Sprintf(format, v...), nil)
 }
 
@@ -66,24 +81,38 @@ func (slf *Logger) Errorf(format string, v ...interface{}) {
 }
 
 func (slf *Logger) Fatalf(format string, v ...interface{}) {
-	slf.LogText(Level_Error, fmt.Sprintf(format, v...), nil)
+	slf.LogText(Level_Fatal, fmt.Sprintf(format, v...), nil)
 	os.Exit(1)
 }
 
 func (slf *Logger) Debugln(v ...interface{}) {
+	if slf.level < globalLevel {
+		return
+	}
 	slf.LogText(Level_Debug, fmt.Sprintln(v...), nil)
 }
 
 func (slf *Logger) Infoln(v ...interface{}) {
+	if slf.level < globalLevel {
+		return
+	}
 	slf.LogText(Level_Info, fmt.Sprintln(v...), nil)
 }
 
 func (slf *Logger) Warnln(v ...interface{}) {
+	if slf.level < globalLevel {
+		return
+	}
 	slf.LogText(Level_Warn, fmt.Sprintln(v...), nil)
 }
 
 func (slf *Logger) Errorln(v ...interface{}) {
 	slf.LogText(Level_Error, fmt.Sprintln(v...), nil)
+}
+
+func (slf *Logger) Fatalln(v ...interface{}) {
+	slf.LogText(Level_Fatal, fmt.Sprintln(v...), nil)
+	os.Exit(1)
 }
 
 func (slf *Logger) SetLevelByString(level string) {
